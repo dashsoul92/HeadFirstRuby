694c03dc7bdbc3075856216c602dc5029adc5fb2
dashsoul92 <dashsoul92@gmail.com>
2017-12-01 17:58:08 -0700

    Updated the employee.rb file. It now has three separate classes to handle hourly employees and salaried employees.

--- employee.rb
+++ employee.rb
@@ -1,7 +1,7 @@
 class Employee
 
-  # Shortcut to create reader accessor methods for both static variables.
-  attr_reader :name, :salary
+  # Shortcut to create reader accessor methods for the static variable.
+  attr_reader :name
 
   # A writer accessor method used to assign a value to the static variable.
   def name=(name)
@@ -12,6 +12,20 @@ class Employee
     end
   end
 
+  def print_name
+    puts "Name: #{name}"
+  end
+
+  def initialize(name = "Anonymous")
+    self.name = name
+  end
+
+end
+
+class SalariedEmployee < Employee
+
+  attr_reader :salary
+
   def salary=(salary)
     if salary < 0
       raise "A salary of #{salary} isn't valid!"
@@ -20,34 +34,66 @@ class Employee
     end
   end
 
+  def print_pay_stub
+    # Here we explicityly specify a receiver using the dot operator, however, if you don't specify a receiver, it'll default to the current object, self.
+    print_name
+    # Finds the daily pay and them multiplies that by the number of days worked in the pay period.
+    # In order to have an accurate calculation, we must divide by 365.00 in order to get a float as a result.
+    pay_for_period = (salary / 365.00) * 14
+    # When using the format method, %s is used for a string, %i is used for integers, and %f is used for floating-point decimals.
+    puts format("Pay this period: $%0.2f", pay_for_period), ""
+  end
+
   # The initialize method is called directly after Ruby allocates memory to hold the new class object.
   # Here we're overriding the superclass, Object's initialize method, and assigning values to the class object's instance variables.
   # This Ruby method is the equivalent of other OOP languages constructor method.
   def initialize(name = "Anonymous", salary = 0.0)
     # The self method refers to the current object.
     # Here we're calling the current object's name and salary methods to validate user input and assign the values to the object's static variables.
-    self.name = name
+    # We only need to use the self method when dealing with writer accessor methods.
+    super(name)
     self.salary = salary
   end
 
+end
+
+class HourlyEmployee < Employee
+
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "An hourly_wage of #{hourly_wage} is invalid!"
+    else
+      @hourly_wage = hourly_wage
+    end
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "#{hours_per_week} is invalid!"
+    else
+      @hours_per_week = hours_per_week
+    end
+  end
+
   def print_pay_stub
-    puts "Name: #{@name}"
-    # Finds the daily pay and them multiplies that by the number of days worked in the pay period.
-    # In order to have an accurate calculation, we must divide by 365.00 in order to get a float as a result.
-    pay_for_period = (@salary / 365.00) * 14
-    # When using the format method, %s is used for a string, %i is used for integers, and %f is used for floating-point decimals.
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
     puts format("Pay this period: $%0.2f", pay_for_period), ""
+  end
+
+  def initialize(name = "Anonymous", hourly_wage = 0.00, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
   end
 
 end
 
 # Any time the new method is called, the initialize method is also called.
-amy = Employee.new
-amy.print_pay_stub
-amy.name = "Amy"
-amy.salary = 50000
-amy.print_pay_stub
+bob = SalariedEmployee.new("Bob", 50000)
+bob.print_pay_stub
 
-bob = Employee.new("Bob", 10000).print_pay_stub
-
-rick = Employee.new("Rick").print_pay_stub
+burt = HourlyEmployee.new("Burt", 11.00, 40.00)
+burt.print_pay_stub
